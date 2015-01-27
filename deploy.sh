4c2cd47da77aa9ed7de5e5d83da151a6c4411536
jwu <jiangwu1911@hotmail.com>
2015-01-27 11:45:47 -0500

    added create vm from nova

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ export DEIS_NUM_INSTANCES=3
 export DEIS_CIDR=10.21.12.0/24
 export DEIS_DNS=10.21.12.3,8.8.8.8
 
-( cd deisdir
+( cd ../deis
   make discovery-url
   sed -i 's#Environment=.*#Environment="DOCKER_OPTS=--insecure-registry 10.0.0.0/8 --insecure-registry 172.16.0.0/12 --insecure-registry 192.168.0.0/16 --insecure-registry 100.64.0.0/10 --insecure-registry 54.65.178.226:5000 --registry-mirror=http://54.65.178.226:5000"#' contrib/coreos/user-data
   sed -i 's#peer-election-timeout:.*#peer-election-timeout: 20000#' contrib/coreos/user-data
   sed -i 's#peer-heartbeat-interval:.*#peer-election-timeout: 5000#' contrib/coreos/user-data
 )
 
-( cd  deisdir/contrib/openstack
+( cd  ../deis/contrib/openstack
   ./provision-openstack-cluster.sh coreos deis m1.medium
 )
 
